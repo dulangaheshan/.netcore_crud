adbffc7a7eb358d5d89cacaa60b8430fe16edb57
Dulanga Heshan <164056m@uom.lk>
2018-09-06 21:48:06 +0530

    Hash and Salt

--- Testing2/DataProvider/DataProvider.cs
+++ Testing2/DataProvider/DataProvider.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Dapper;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using Testing2.Hash_salt;
 using Testing2.Model;
+
 
 namespace Testing2.DataProvider
 {
     public class DataProvider : IDataProvider
     {
+        String detail;
+        private String check_mail;
+        private String check_pass;
         private readonly String connectionString;
         public DataProvider()
         {
-            connectionString = "Server=DESKTOP-ALMQ9QA\\SQLEXPRESS;Database=handallo;Trusted_Connection=True;MultipleActiveResultSets=true"; ;
+            connectionString = "Server=DESKTOP-ALMQ9QA\\SQLEXPRESS;Database=handallo;Trusted_Connection=True;MultipleActiveResultSets=true";
         }
 
         public IDbConnection Connection
@@ -31,26 +41,75 @@ namespace Testing2.DataProvider
         {
             using (IDbConnection dbConnection = Connection)
             {
-                string sQuery = "SELECT * FROM Customers"
+                string sQuery = "SELECT * FROM Customer"
                                 + " WHERE CustomerId = @Id";
                 dbConnection.Open();
+               
                 return dbConnection.Query<User>(sQuery, new {Id = CustomerId}).FirstOrDefault();
             }
         }
 
-        public User AddUser(User user)
+        public Boolean AddUser(User user)
         {
+            var email = user.Email;
+
+            user.Passw = HashSalt.Hash_Salt(user.Passw);
+
             using (IDbConnection dbConnection = Connection)
             {
-                string sQuery = "INSERT INTO Customers(FirstName,LastName,Passw,Email)" +
-                                "VALUES(@firstName,@lastName,@passw,@email)";
+                string sQuery0 = "SELECT FirstName FROM Customer WHERE Email = @email";
 
                 dbConnection.Open();
-                dbConnection.Execute(sQuery, user);
-                return user;
+                string result = dbConnection.QueryFirstOrDefault<String>(sQuery0, new { @Email = email });
+                dbConnection.Close();
+
+                if (String.IsNullOrEmpty(result))
+                {
+                    string sQuery = "INSERT INTO Customer(FirstName,LastName,Passw,Email)" +
+                                    "VALUES(@firstName,@lastName,@passw,@email)";
+
+                    dbConnection.Open();
+                    dbConnection.Execute(sQuery, user);
+                    return true;
+
+                }
+
+                else
+                {
+                    return false;
+                }
+
 
             }
         }
+
+        public Boolean UserLogin(Login login)
+        {
+
+
+            login.Password = HashSalt.Hash_Salt(login.Password);
+
+            var o = login.Email;
+            var i = login.Password;
+            
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = "SELECT FirstName FROM Customer WHERE Email = @Email AND Passw = @Password";
+                dbConnection.Open();
+                detail = dbConnection.QueryFirstOrDefault<String>(sQuery,new{@Email = o , @Password = i});
+
+
+            }
+
+            if (String.IsNullOrEmpty(detail))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        } 
 
 
     }
---
+++ Testing2/Hash_salt/Hash_Salt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Testing2.Hash_salt 
+{
+    public static class HashSalt
+    {
+        public static string Hash_Salt(String password)
+        {
+            // generate a 128-bit salt using a secure PRNG
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            //String newsalt = Convert.ToBase64String(salt);
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+
+
+            return hashed;
+        }
+
+
+    }
+
+}
